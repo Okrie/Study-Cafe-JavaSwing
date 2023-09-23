6e56e68a18366b11295774ed6d1543d803e9d251
Okrie <mykkang55@naver.com>
2023-09-23 22:44:42 +0900

    Add files via upload

---
+++ src/com/javalec/util/ShareVar.java
@@ -0,0 +1,14 @@
+package com.javalec.util;
+
+public class ShareVar {
+
+		public static final String DBNAME = "jdbc:mysql:?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+		public static final String DBUSER = "";
+		public static final String DBPASS = "";
+		
+		public static String id;
+		
+		public static String labelText; // 좌석번호
+		public static String priceCategory; // 카테고리
+		public static int pPrice; // 결제가격
+}
